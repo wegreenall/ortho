06b97290a17bc1ebd1ae0066c263bc4b07c0904c
wegreenall <wjgreenall@gmail.com>
2022-05-10 08:27:14 +0300

    added builders and builders tests

---
+++ builders.py
@@ -0,0 +1,150 @@
+import torch
+import torch.distributions as D
+from ortho.measure import MaximalEntropyDensity
+from ortho.orthopoly import (
+    SymmetricOrthogonalPolynomial,
+    SymmetricOrthonormalPolynomial,
+)
+from ortho.utils import sample_from_function, integrate_function, gauss_moment
+from typing import Callable
+
+"""
+This file contains builder functions for various components
+required for building out the Favard kernel.
+"""
+
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+
+def get_weight_function_from_sample(
+    sample: torch.Tensor, order
+) -> "MaximalEntropyDensity":
+    """
+    For a given sample, returns the maximal entropy weight function that
+    correponds to the moments that the sample has - i.e. it creates the
+    maximal entropy weight function corresponding to the weight function.
+    """
+    gammas = get_gammas_from_sample(sample, 2 * order)
+    return MaximalEntropyDensity(order, torch.zeros(2 * order), gammas)
+
+
+def get_moments_from_function(
+    target: Callable,
+    end_point: torch.Tensor,
+    func_max: torch.Tensor,
+    order: int,
+    sample_size=2000 ** 2,
+):
+    sample = sample_from_function(
+        target, end_point, func_max, sample_size
+    ).squeeze()
+    return get_moments_from_sample(sample, order)
+
+
+def get_moments_from_sample(sample: torch.Tensor, order: int) -> torch.Tensor:
+    """
+    Returns a sequence of moments calculated from a sample.
+    The odd-ordered moments are set to 0 to handle the fact that we are
+    calculating a symmetric orthogonal polynomial (we only have one equation
+    for each parameter that we want to solve for.
+    """
+    powers_of_sample = sample.repeat(2 * order + 2, 1).t() ** torch.linspace(
+        0, 2 * order + 1, 2 * order + 2
+    )
+    estimated_moments = torch.mean(powers_of_sample, dim=0)
+
+    # build moments
+    moments = torch.zeros(2 * order + 2)
+    moments[0] = 1
+    for i in range(1, 2 * order + 2):
+        if i % 2 == 0:  # i.e. even
+            moments[i] = estimated_moments[i]
+    # breakpoint()
+    return moments
+
+
+def get_gammas_from_moments(moments: torch.Tensor, order: int) -> torch.Tensor:
+    """
+    Accepts a tensor containing the moments from a given
+    distribution, and generates the gammas that correspond to them;
+    i.e., the gammas from the orthogonal polynomial series that
+    is orthogonal w.r.t the linear moment functional with those moments.
+    """
+    dets = torch.zeros(order + 2)
+    dets[0] = dets[1] = 1.0
+    gammas = torch.zeros(order)
+    for i in range(order):
+        hankel_matrix = moments[: 2 * i + 1].unfold(0, i + 1, 1)
+        dets[i + 2] = torch.linalg.det(hankel_matrix)
+    gammas = dets[:-2] * dets[2:] / (dets[1:-1] ** 2)
+    return gammas
+
+
+def get_poly_from_moments(
+    moments: torch.Tensor,
+    order: int,
+) -> SymmetricOrthonormalPolynomial:
+    """
+    Accepts a list of moment values and produces from it a
+    SymmetricOrthogonalPolynomial.
+
+    The standard recurrence for an orthogonal polynomial series has n equations
+    and 2n unknowns - this means that it is not feasible to construct, from a
+    given sequence of moments, an orthogonal polynomial sequence that is
+    orthogonal     w.r.t the given moments. However, if we impose
+    symmetricality, we can build a sequence of symmetric orthogonal polynomials
+    from a given set of moments.
+    """
+    gammas = torch.zeros(order)
+
+    # to construct the polynomial from the sequnce of moments, utilise the
+    # sequence of equations:
+    gammas = get_gammas_from_moments(moments, order)
+
+    return SymmetricOrthonormalPolynomial(order, gammas)
+
+
+def get_gammas_from_sample(sample: torch.Tensor, order: int) -> torch.Tensor:
+    """
+    Composes get_gammas_from_moments and get_moments_from_sample to
+    produce the gammas from a sample. This just allows for simple
+    calls to individual functions to construct the necessary
+    component in any given situation.
+    """
+    return get_gammas_from_moments(
+        get_moments_from_sample(sample, order), order
+    )
+
+
+def get_poly_from_sample(
+    sample: torch.Tensor, order: int
+) -> SymmetricOrthonormalPolynomial:
+    """
+    Returns a SymmetricOrthogonalPolynomial calculated by:
+         - taking the moments from the sample, with odd moments set to 0;
+         - constructing from these the gammas that correspond to the
+           SymmetricOrthogonalPolynomial recursion
+         - generating the SymmetricOrthogonalPolynomial from these gammas.
+    Hence we have a composition of the three functions:
+          get_moments_from_sample -> get_poly_from_moments
+    """
+    moments = get_moments_from_sample(sample, order)
+    return get_poly_from_moments(moments, order)
+
+
+if __name__ == "__main__":
+    dist = D.Normal(0.0, 1.0)
+    sample = dist.sample([4000 ** 2])
+    order = 10
+    checked_moments = get_moments_from_sample(sample, order)[: order + 2]
+    print(
+        "Should be like standard normal moments:",
+    )
+
+    # normal moments:
+    normal_moments = []
+    for n in range(1, 2 * order + 2):
+        normal_moments.append(gauss_moment(n))
+    normal_moments = torch.Tensor(normal_moments)[: order + 1]
+    # breakpoint()
+    print(checked_moments[2:] / normal_moments[1:])
---
+++ test/test_builders.py
@@ -0,0 +1,154 @@
+import torch
+import torch.distributions as D
+from ortho.builders import (
+    get_weight_function_from_sample,
+    get_moments_from_function,
+    get_moments_from_sample,
+    get_gammas_from_moments,
+    integrate_function,
+    sample_from_function,
+    get_poly_from_moments,
+    get_gammas_from_sample,
+    get_poly_from_sample,
+)
+import math
+
+# from ortho.measure import coeffics_list
+import unittest
+import matplotlib.pyplot as plt
+
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+torch.set_printoptions(precision=10)
+
+
+def function_for_sampling(x: torch.Tensor):
+    return (1 / (math.sqrt(math.pi * 2))) * torch.exp(-(x ** 2) / 2)
+
+
+def gauss_moment(n: int) -> int:
+    """
+    Returns the n-th moment of a standard normal Gaussian distribution.
+    """
+    if n == 0:
+        return 1
+    if n % 2 == 0:
+        return double_fact(n - 1)
+    else:
+        return 0
+
+
+def double_fact(n: int) -> int:
+    """
+    Evaluates the double factorial of the input integer n
+    """
+    # assert n % 2 == 0, "n is not even!"
+    # p = n - 1
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return n * double_fact(n - 2)
+
+
+class TestBuilders(unittest.TestCase):
+    def setUp(self):
+        self.order = 10
+        # self.coeffics = torch.Tensor([0.0, 0.5, 3.5, -4, -20])
+        # self.roots = torch.Tensor([-0.4617, -0.1361, 0, 0.3978])
+        # self.peaks = torch.Tensor([-0.3493, -0.0672, 0.2664])
+        distribution = D.Normal(0.0, 1.0)
+        sample_size = 10000000
+        self.sample = distribution.sample((sample_size,))
+
+        self.end_point = torch.tensor(10.0)
+        fineness = 1000
+        self.input_points = torch.linspace(
+            -self.end_point, self.end_point, fineness
+        )
+        # normal moments:
+        normal_moments = []
+        for n in range(2 * self.order + 2):
+            normal_moments.append(gauss_moment(n))
+        self.normal_moments = torch.Tensor(normal_moments)
+
+        self.prob_polynomials = [
+            lambda x: torch.ones(x.shape),
+            lambda x: x,
+            lambda x: x ** 2 - 1,
+            lambda x: x ** 3 - 3 * x,
+            lambda x: x ** 4 - 6 * x ** 2 + 3,
+            lambda x: x ** 5 - 10 * x ** 3 + 15 * x,
+        ]
+
+    # def test_get_weight_function_from_sample(self):
+    # weight_function = get_weight_function_from_sample(
+    # self.coeffics, self.order)
+    # moments = weight_function._get_moments()
+    # self.assertTrue(torch.allclose(moments, self.normal_moments))
+
+    def test_get_moments_from_sample(self):
+        calculated_moments = get_moments_from_sample(self.sample, self.order)
+        breakpoint()
+        self.assertTrue(
+            torch.allclose(calculated_moments, self.normal_moments)
+        )
+
+    def test_get_gammas_from_moments(self):
+        moments = self.normal_moments
+        gammas = get_gammas_from_moments(moments, self.order)
+        self.assertTrue(
+            torch.allclose(
+                gammas, torch.linspace(0.0, self.order - 1, self.order), 1e-02
+            )
+        )
+
+    def test_get_gammas_from_sample(self):
+        gammas = get_gammas_from_sample(self.sample, self.order)[1:]
+        comparison_gammas = torch.linspace(1.0, self.order - 1, self.order - 1)
+        # breakpoint()
+        self.assertTrue(torch.allclose(gammas, comparison_gammas), 3e-2)
+
+    def test_integrate_function(self):
+        # breakpoint()
+        integral = integrate_function(
+            function_for_sampling,
+            self.end_point,
+            (1 / (torch.sqrt(math.pi * 2))),
+        )
+        self.assertTrue(torch.allclose(integral, 1))
+
+    def test_sample_from_function(self):
+        new_sample = sample_from_function(
+            function_for_sampling,
+            self.end_point,
+            (1 / (torch.sqrt(torch.tensor(math.pi * 2)))),
+        )
+        calculated_moments = get_moments_from_sample(new_sample, self.order)
+        breakpoint()
+        self.assertTrue(
+            torch.allclose(calculated_moments, self.normal_moments, rtol=1e-01)
+        )
+
+    def test_get_poly_from_moments(self):
+        poly = get_poly_from_moments(self.normal_moments, self.order)
+        for i in range(6):
+            with self.subTest(i=i):
+                polyvals = poly(self.input_points, i, None)
+                true_polyvals = self.prob_polynomials[i](self.input_points)
+                self.assertTrue(torch.allclose(polyvals, true_polyvals))
+
+    def test_get_poly_from_sample(self):
+        poly = get_poly_from_sample(self.sample, self.order)
+        # breakpoint()
+        for i in range(6):
+            with self.subTest(i=i):
+                polyvals = poly(self.input_points, i, None)
+                true_polyvals = self.prob_polynomials[i](self.input_points)
+                # breakpoint()
+                self.assertTrue(torch.allclose(polyvals, true_polyvals))
+
+
+if __name__ == "__main__":
+    unittest.main()
